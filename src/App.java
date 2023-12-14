853b29da79a198d10e2eb34afdaae37f358dfdba
dsarotti <sr.sarotti@gmail.com>
2023-12-14 20:59:09 +0100

    primer commit

--- src/App.java
+++ src/App.java
@@ -1,13 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         ConexionBD conexionBD= ConexionBD.getConexionBD();
-        if(conexionBD!=null){
-            System.out.println("Existe conexión!");
-        }else{
-            System.out.println("La conexion no está creada :(");
-        }
         
-        System.out.println(conexionBD.getConnection().isValid(200)?"Existe conexión!":"La conexion no está creada :(");
+        System.out.println(
+            conexionBD.getConnection().isValid(200)?"Existe conexión!"
+            :
+            "La conexion no está creada :("
+        );
         
 
         conexionBD.cerrarConexion();
